284f3e0442526ad32f04bbc86bcec64aeb447419
도예 <kmdy125@gmail.com>
2023-08-09 23:53:08 +0900

    test

--- ds.js
+++ ds.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB6QYzllwL-ENx9iPBYzg7Pntc6R0RfCfo",
-  authDomain: "doit-blahx2.firebaseapp.com",
-  projectId: "doit-blahx2",
-  storageBucket: "doit-blahx2.appspot.com",
-  messagingSenderId: "683560115293",
-  appId: "1:683560115293:web:fbf6512d5e6d33e2b981b4",
-  measurementId: "G-ZXS33PW1LZ"
+  apiKey: 'AIzaSyB6QYzllwL-ENx9iPBYzg7Pntc6R0RfCfo',
+  authDomain: 'doit-blahx2.firebaseapp.com',
+  projectId: 'doit-blahx2',
+  storageBucket: 'doit-blahx2.appspot.com',
+  messagingSenderId: '683560115293',
+  appId: '1:683560115293:web:fbf6512d5e6d33e2b981b4',
+  measurementId: 'G-ZXS33PW1LZ',
 };
